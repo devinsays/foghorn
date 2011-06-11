626a787c2ec2f577ef43a01d5b07acc940b3d103
Devin Price <devin@wptheming.com>
2011-06-11 16:32:14 -0500

    Restyling the header area.

--- header.php
+++ header.php
@@ -69,7 +69,7 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed">
-	<header id="branding" role="banner">
+	<header id="branding" role="banner" class="clearfix">
 			<hgroup>
 				<h1 id="site-title"><span><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span></h1>
 				<h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
