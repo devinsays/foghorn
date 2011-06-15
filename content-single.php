9b602595d911b8e74e5f10d782f322497a42b108
Devin Price <devin@wptheming.com>
2011-06-15 00:06:56 -0500

    Slow but steady.

--- content-single.php
+++ content-single.php
@@ -11,6 +11,19 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<h1 class="entry-title"><?php the_title(); ?></h1>
+
+		<div class="entry-meta">
+			<?php
+				printf( __( '<span class="sep">Posted on </span><a href="%1$s" rel="bookmark"><time class="entry-date" datetime="%2$s" pubdate>%3$s</time></a> <span class="sep"> by </span> <span class="author vcard"><a class="url fn n" href="%4$s" title="%5$s">%6$s</a></span>', 'twentyeleven' ),
+					get_permalink(),
+					get_the_date( 'c' ),
+					get_the_date(),
+					get_author_posts_url( get_the_author_meta( 'ID' ) ),
+					sprintf( esc_attr__( 'View all posts by %s', 'twentyeleven' ), get_the_author() ),
+					get_the_author()
+				);
+			?>
+		</div><!-- .entry-meta -->
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
@@ -36,6 +49,7 @@
 				get_author_posts_url( get_the_author_meta( 'ID' ) )
 			);
 		?>
+		<?php edit_post_link( __( 'Edit', 'twentyeleven' ), '<span class="edit-link">', '</span>' ); ?>
 
 		<?php if ( get_the_author_meta( 'description' ) ) : // If a user has filled out their description, show a bio on their entries ?>
 		<div id="author-info">
--- single.php
+++ single.php
@@ -10,6 +10,7 @@
 get_header(); ?>
 
 		<div id="primary">
+            
 			<div id="content" role="main">
 
 				<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
@@ -29,22 +30,6 @@ get_header(); ?>
 				<?php endwhile; // end of the loop. ?>
 
 			</div><!-- #content -->
-            
-            <aside id="entry-meta" role="complementary">
-                <p><?php
-                    printf( __( '<span class="sep">Posted on </span><a href="%1$s" rel="bookmark"><time class="entry-date" datetime="%2$s" pubdate>%3$s</time></a> <span class="sep"> by </span> <span class="author vcard"><a class="url fn n" href="%4$s" title="%5$s">%6$s</a></span>', 'twentyeleven' ),
-                        get_permalink(),
-                        get_the_date( 'c' ),
-                        get_the_date(),
-                        get_author_posts_url( get_the_author_meta( 'ID' ) ),
-                        sprintf( esc_attr__( 'View all posts by %s', 'twentyeleven' ), get_the_author() ),
-                        get_the_author()
-                    );
-                ?></p>
-                <p>More on that in a minute.</p>
-                <p><?php edit_post_link( __( 'Edit', 'twentyeleven' ), '<span class="edit-link">', '</span>' ); ?></p>
-            </aside> <!-- #entry-meta -->
-        
 		</div><!-- #primary -->
         
 <?php get_sidebar(); ?>
